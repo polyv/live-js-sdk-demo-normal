1079f3b495927c0757de9e77d230d71880803472
H-zk <zikai@imagedt.com>
2022-08-04 16:15:56 +0800

    chore: 使用 @polyv/eslint-config 作为 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,7 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  extends: [
+    'eslint:recommended',
+    './node_modules/@polyv/eslint-config/lib/for-vue'
+  ]
+};
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## 关联仓库
 
 - [live-js-sdk-demo-basic](https://github.com/polyv/live-js-sdk-demo-basic)
+
+## 说明
+
+- 基于 `node@16.9.1` 搭建
