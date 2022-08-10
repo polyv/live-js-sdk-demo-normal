4cc8597efd4e5d7f2acab4a4fe3bef47edcab3c9
H-zk <zikai@imagedt.com>
2022-08-10 13:15:03 +0800

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     './node_modules/@polyv/eslint-config/lib/for-vue'
   ],
   rules: {
-    'vue/multi-word-component-names': 0
+    'vue/multi-word-component-names': 0,
+    'no-unused-vars': 0,
+    'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }]
   }
 };
