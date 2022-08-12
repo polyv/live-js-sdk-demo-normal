3211cf65e182aa7a291bc5a83a0893bcf831750b
H-zk <zikai@imagedt.com>
2022-08-12 16:21:28 +0800

    feat: 增加互动组件-抽奖 Lottery

--- src/components/interactions-receive/Entrance.vue
+++ src/components/interactions-receive/Entrance.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    <announcement-entrance />
+  <div class="interactions-receive-entrance">
+    <AnnouncementEntrance />
+    <LotteryEntrance />
   </div>
 </template>
 
 <script>
 import AnnouncementEntrance from '@/components/interactions-receive/Announcement';
+import LotteryEntrance from '@/components/interactions-receive/Lottery';
 
 export default {
   components: {
     AnnouncementEntrance,
+    LotteryEntrance,
   },
 };
 </script>
+
+<style lang="scss">
+.interactions-receive-entrance {
+  position: relative;
+  /* 需要大于播放器控制条的 z-inedx */
+  z-index: 2001;
+}
+</style>
---
+++ src/components/interactions-receive/Lottery/MobileLottery.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="plv-demo-lottery-default">
+    <button @click="setLotteryRecordVisible">查看中奖记录</button>
+
+    <div class="plv-demo-lottery-default__lottery">
+      <!-- 抽奖中 -->
+      <on-lottery v-if="lotterySdk"
+                  v-show="isLotteryShowing"
+                  :lottery-sdk="lotterySdk"
+                  :lang="lang"
+                  @lottery-status-changed="onLotteryStatusChange"
+                  @is-show-changed="onLotteryShowChange" />
+    </div>
+
+    <!-- 中奖记录 -->
+    <modal no-bg
+           draggable
+           title="中奖记录"
+           :visible="isShowRecord"
+           :close-on-click-modal="false"
+           @close="isShowRecord = false">
+      <lottery-record v-if="lotterySdk"
+                      :lottery-sdk="lotterySdk"
+                      :lang="lang"
+                      :delay-time="3000"
+                      @lottery-list="onLotteryRecord"
+                      @submit-info="onClickRecord"
+                      @check-info="onClickRecord" />
+    </modal>
+
+    <!-- 中奖结果 -->
+    <!-- modal是一个弹窗组件，可根据界面风格自行设计-->
+    <modal no-bg
+           draggable
+           title="中奖结果"
+           :visible="isShowResult"
+           :close-on-click-modal="false"
+           :lang="lang"
+           @close="isShowResult = false">
+      <lottery-end v-if="lotterySdk"
+                   ref="lotteryEnd"
+                   :lottery-sdk="lotterySdk"
+                   :lottery-list="lotteryList"
+                   :lang="lang"
+                   @to-show="setLotteryResultShow"
+                   @to-hide="setLotteryResultHide" />
+    </modal>
+  </div>
+</template>
+
+<script>
+import OnLottery from '@polyv/interactions-receive-sdk-ui-default/lib/MobileOnLottery';
+import LotteryEnd from '@polyv/interactions-receive-sdk-ui-default/lib/MobileLotteryEnd';
+import LotteryRecord from '@polyv/interactions-receive-sdk-ui-default/lib/MobileLotteryRecord';
+import { Lottery } from '@polyv/interactions-receive-sdk';
+
+export default {
+  components: {
+    OnLottery,
+    LotteryEnd,
+    LotteryRecord,
+  },
+
+  data() {
+    return {
+      // 语言
+      lang: 'zh_CN',
+      // 抽奖 SDK 实例
+      lotterySdk: null,
+      // 是否展示结果
+      isShowResult: false,
+      // 中奖记录数据
+      lotteryList: [],
+      // 是否展示抽奖盒子
+      isLotteryShowing: false,
+      // 是否展示抽奖记录
+      isShowRecord: false,
+    };
+  },
+
+  created() {
+    this.lotterySdk = new Lottery();
+  },
+
+  beforeDestroy() {
+    this.lotterySdk?.destroy();
+    this.lotterySdk = null;
+  },
+
+  methods: {
+    onLotteryStatusChange(status) {
+      if (status === 'running') {
+        this.isLotteryShowing = true;
+      } else if (status === 'over') {
+        this.isLotteryShowing = false;
+      }
+    },
+
+    onLotteryShowChange(isShowing) {
+      this.isLotteryShowing = isShowing;
+    },
+
+    // 展示抽奖结果
+    setLotteryResultShow() {
+      this.isShowResult = true;
+    },
+
+    // 隐藏抽奖结果
+    setLotteryResultHide() {
+      this.isShowResult = false;
+      this.$refs.lotteryEnd && this.$refs.lotteryEnd.toBack();
+    },
+
+    // 切换中奖记录列表组件可见性
+    setLotteryRecordVisible() {
+      this.isShowRecord = !this.isShowRecord;
+    },
+
+    // 中奖记录数据更新
+    onLotteryRecord(lotteryList = []) {
+      if (lotteryList.length) {
+        this.lotteryList = lotteryList;
+      }
+    },
+
+    // 点击查看中奖结果详情
+    onClickRecord(record = {}) {
+      this.isShowRecord = false;
+      const { prize, lotteryId, collectInfo, winnerCode, sessionId, received } =
+        record;
+      this.$refs.lotteryEnd.setLottery({
+        received,
+        prize,
+        lotteryId,
+        collectInfo,
+        winnerCode,
+        sessionId,
+        isWinner: true,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.plv-demo-lottery-default__lottery {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
